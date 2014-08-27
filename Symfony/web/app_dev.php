93dcfb61cb2385685fb82184593995abd3d642cb
francis <franchesco971@gmail.com>
2014-08-27 18:56:47 +0200

    date et ip

--- Symfony/web/app_dev.php
+++ Symfony/web/app_dev.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1','88.176.12.225'))
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1','82.229.222.236'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
