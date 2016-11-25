9bddc86e34013f27ab41da87ec41e740b96845c1
franchesco971 <franchesco971@gmail.com>
2016-11-26 00:32:15 +0100

    itw bundle

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             new Spicy\TagBundle\SpicyTagBundle(),
             new Spicy\RankingBundle\SpicyRankingBundle(),
             new Spicy\AppBundle\SpicyAppBundle(),
+            new Spicy\ITWBundle\SpicyITWBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
