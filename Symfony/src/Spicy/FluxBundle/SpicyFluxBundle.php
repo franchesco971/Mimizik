60ed14eb2aa95e4267de1f71ce0f8681f53ac7b6
francis copol <franciscopol@Mac-mini-de-Maximilien.local>
2017-10-06 10:33:46 +0200

    suppression du dossier symfony

--- Symfony/src/Spicy/FluxBundle/SpicyFluxBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Spicy\FluxBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SpicyFluxBundle extends Bundle
-{
-}
--- Symfony/src/Spicy/LyricsBundle/SpicyLyricsBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Spicy\LyricsBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SpicyLyricsBundle extends Bundle
-{
-}
